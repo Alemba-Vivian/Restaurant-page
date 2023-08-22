7a3c6641012b4c80fce4f09c53ef322a19d24ca9
Alemba-Vivian <alembavivianauma@gmail.com>
2023-08-22 19:23:21 +0300

    working with the lists

--- main.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-alert("hello my name is vivian");
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLGlDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudC1wYWdlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImFsZXJ0KFwiaGVsbG8gbXkgbmFtZSBpcyB2aXZpYW5cIik7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
